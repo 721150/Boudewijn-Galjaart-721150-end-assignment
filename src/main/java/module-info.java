a7cf51a00e8ce242d0b9d3b0f2cc07766c02de8c
721150 <144417455+721150@users.noreply.github.com>
2024-09-26 13:32:50 +0200

    Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module nl.inholland.javafundamentals.boudewijngaljaart721150endassignment {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens nl.inholland.javafundamentals.boudewijngaljaart721150endassignment to javafx.fxml;
+    exports nl.inholland.javafundamentals.boudewijngaljaart721150endassignment;
+}
