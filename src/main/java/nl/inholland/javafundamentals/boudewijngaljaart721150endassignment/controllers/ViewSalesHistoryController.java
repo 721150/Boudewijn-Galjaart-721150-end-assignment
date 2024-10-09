8111f29f42af109f032d4d48401912dbd4c01124
721150 <144417455+721150@users.noreply.github.com>
2024-10-09 18:48:10 +0200

    v20
    Logica voor het tonen van de verkochte kaartjes toegevoegd

--- src/main/java/nl/inholland/javafundamentals/boudewijngaljaart721150endassignment/controllers/ViewSalesHistoryController.java
+++ src/main/java/nl/inholland/javafundamentals/boudewijngaljaart721150endassignment/controllers/ViewSalesHistoryController.java
@@ -1,15 +1,36 @@
 package nl.inholland.javafundamentals.boudewijngaljaart721150endassignment.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
 import nl.inholland.javafundamentals.boudewijngaljaart721150endassignment.data.Database;
+import nl.inholland.javafundamentals.boudewijngaljaart721150endassignment.models.Show;
 import nl.inholland.javafundamentals.boudewijngaljaart721150endassignment.models.User;
 
-public class ViewSalesHistoryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewSalesHistoryController implements Initializable {
     private User user;
 
     private Database database;
 
+    private ObservableList<Show> shows = FXCollections.observableArrayList();
+
+    @FXML
+    private TableView showingsTableInformationTebleView;
+
     public void giveData(User user, Database database) {
         this.user = user;
         this.database = database;
+        this.shows.setAll(this.database.getShows());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Laat de gegevens van de verkochte voorstellingen weergeven op het scherm
+        showingsTableInformationTebleView.setItems(shows);
     }
 }
